1866f48965b2f269dbf4530c36a53cb179cd2219
PumpkinSeed <qwer.kocka@gmail.com>
2022-03-24 20:46:30 +0100

    Finish package

--- crypto/encrypter.go
+++ crypto/encrypter.go
@@ -3,7 +3,7 @@ package crypto
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
+	"strings"
 )
 
 const (
@@ -16,8 +16,12 @@ type Encrypter interface {
 }
 
 type result struct {
-	Version         string   `json:"version"`
-	Length          uint32   `json:"length"`
+	Version string    `json:"version"`
+	Length  uint32    `json:"length"`
+	V1      *resultV1 `json:"v1,omitempty"`
+}
+
+type resultV1 struct {
 	EncryptedChunks []string `json:"encrypted_chunks"`
 	chunks          []string
 }
@@ -41,16 +45,60 @@ func (e encrypter) Encrypt(data string) (string, error) {
 }
 
 func (e encrypter) Decrypt(data string) (string, error) {
+	dataJSON, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return "", err
+	}
+	var r result
+	if err := json.Unmarshal([]byte(dataJSON), &r); err != nil {
+		return "", err
+	}
+
+	switch r.Version {
+	case EncrypterVersion1:
+		return e.decryptV1(r.V1, r.Length)
+	}
+
 	return "", nil
 }
 
 func (e encrypter) encryptV1(data string) (result, error) {
-	var r = result{}
-	r.chunks = e.splitToChunks(data, 16)
+	var r = result{
+		Version: EncrypterVersion1,
+		Length:  uint32(len(data)),
+		V1:      &resultV1{},
+	}
+	r.V1.chunks = e.splitToChunks(data, 16)
 	r.Length = uint32(len(data))
-	fmt.Println(r.chunks)
+
+	for _, chunk := range r.V1.chunks {
+		if len(chunk) != 16 {
+			chunk = e.appendTo(chunk, 16)
+		}
+		encryptedChunk, err := AES128{}.Encrypt(chunk, e.key, []byte(e.iv))
+		if err != nil {
+			return result{}, err
+		}
+		r.V1.EncryptedChunks = append(r.V1.EncryptedChunks, base64.StdEncoding.EncodeToString([]byte(encryptedChunk)))
+	}
 
 	return r, nil
+}
+
+func (e encrypter) decryptV1(data *resultV1, length uint32) (string, error) {
+	for _, chunk := range data.EncryptedChunks {
+		decoded, err := base64.StdEncoding.DecodeString(chunk)
+		if err != nil {
+			return "", err
+		}
+		decrypted, err := AES128{}.Decrypt(string(decoded), e.key, []byte(e.iv))
+		if err != nil {
+			return "", err
+		}
+		data.chunks = append(data.chunks, decrypted)
+	}
+	res := strings.Join(data.chunks, "")
+	return res[0:length], nil
 }
 
 func (e encrypter) splitToChunks(s string, n int) []string {
@@ -64,4 +112,14 @@ func (e encrypter) splitToChunks(s string, n int) []string {
 	}
 	result = append(result, s)
 	return result
+}
+
+func (e encrypter) appendTo(chunk string, n int) string {
+	var appendN = ""
+
+	for i := 0; i < n-len(chunk); i++ {
+		appendN += "x"
+	}
+
+	return chunk + appendN
 }
--- crypto/encrypter_test.go
+++ crypto/encrypter_test.go
@@ -1,10 +1,25 @@
 package crypto
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestEncrypter_Encrypt(t *testing.T) {
-	encrypter{
-		key: "test",
-		iv:  "test",
-	}.Encrypt("testdatatestdatatestdatatestdatatestdatatestdata1234")
+	dataToEncrypt := "testdatatestdatatestdatatestdatatestdatatestdata1234"
+	e := encrypter{
+		key: "bhXRirFB8IaQxxjm",
+		iv:  "oZDRWCHryRtlVA1I",
+	}
+	resultData, err := e.Encrypt(dataToEncrypt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	decrypted, err := e.Decrypt(resultData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if decrypted != dataToEncrypt {
+		t.Errorf("Decrypted is not the original data, original: %s, decrypted: %s", dataToEncrypt, decrypted)
+	}
 }
