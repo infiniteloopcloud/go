24f62fdaad0dd2ba436ed22705cd24b353c2a90d
Robert Boros <robbie13.boros@gmail.com>
2023-06-07 16:20:55 +0200

    add comment to custom writer

--- middlewares/custom_writer.go
+++ middlewares/custom_writer.go
@@ -6,6 +6,7 @@ import (
 	"github.com/infiniteloopcloud/hyper"
 )
 
+// CustomWriter wraps the http.ResponseWriter into a new hyper.Writer
 func CustomWriter() func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
