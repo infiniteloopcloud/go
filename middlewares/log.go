ab7f1cb2348f48b8fbf624d3da7b9b83145adde8
Robert Boros <robbie13.boros@gmail.com>
2023-06-07 15:24:40 +0200

    revert custom log

--- middlewares/log.go
+++ middlewares/log.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/infiniteloopcloud/hyper"
@@ -11,8 +12,9 @@ type LogOpts struct {
 	LogLevel uint8
 }
 
-// SetLogLevel middleware is responsible for set the log level
-func SetLogLevel(opts ...LogOpts) func(next http.Handler) http.Handler {
+// CustomLog middleware is responsible for setting the logLevel,
+// and also logs the response body
+func CustomLog(opts ...LogOpts) func(next http.Handler) http.Handler {
 	var o LogOpts
 	if len(opts) == 1 {
 		o = opts[0]
@@ -20,8 +22,12 @@ func SetLogLevel(opts ...LogOpts) func(next http.Handler) http.Handler {
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
 			log.SetLevel(o.LogLevel)
-			next.ServeHTTP(w, r)
+			ctx = context.WithValue(ctx, log.HTTPPath, r.RequestURI)
+			rw := hyper.NewWriter(w)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(rw, r)
 		})
 	}
 }
